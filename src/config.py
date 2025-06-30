69fca3f8470aff032e15e35c54f264a4272d23f9
Marco <marco@mdigennaro.net>
2025-06-30 13:58:58 +0200

    restructure

--- src/config.py
+++ src/config.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 # Get the base directory (directory where this file resides)
-BASE_DIR = Path(__file__).parent
+BASE_DIR = Path(__file__).parent.parent
 
 # Paths to various resources
 DATA_DIR = BASE_DIR / 'data'
