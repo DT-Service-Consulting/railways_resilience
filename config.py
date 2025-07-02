84f68ee589944bcf23ef5604b8c091bdd980494a
Marco <marco@mdigennaro.net>
2025-07-02 15:56:11 +0200

    data folder

--- config.py
+++ config.py
@@ -1,9 +1,7 @@
 from pathlib import Path
 
 # Get the base directory (directory where this file resides)
-BASE_DIR = Path(__file__).parent.parent
+BASE_DIR = Path(__file__)
 
 # Paths to various resources
 DATA_DIR = BASE_DIR / 'data'
-NB_DIR = BASE_DIR / 'notebooks'
-ENVFILE = BASE_DIR / '.env'
