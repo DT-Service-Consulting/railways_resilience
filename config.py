a5caefc5feb4f11a991d215f8b962ab323898446
Marco <marco@mdigennaro.net>
2025-11-07 11:19:21 +0100

    inclued path graphs

--- config.py
+++ config.py
@@ -5,6 +5,8 @@ BASE_DIR = Path(__file__).parent
 
 # Paths to various resources
 DATA_DIR = BASE_DIR / 'data'
+GRAPHS_DIR = BASE_DIR / 'graphs'
+
 
 PATH_TO_SQLITE = DATA_DIR / "sqlite/belgium.sqlite" # Path where the sqlite database is stored
 L_SPACE_PATH = DATA_DIR / "pkl/belgium_routesCleaned.pkl"  # Path where the clean L-space graph was stored (cleaned)
